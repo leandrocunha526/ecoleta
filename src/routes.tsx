c3582b337b81210e9b9aa0df91642102a721b83e
leandrocunha526 <leandrocunha016@gmail.com>
2021-08-10 15:52:22 -0300

    Add create point

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, BrowserRouter } from "react-router-dom";
 
 import Home from "./pages/Home";
-//import CreatePoint from "./pages/Home";
+import CreatePoint from "./pages/CreatePoint";
 
 const Routes = () => {
     return (
         <BrowserRouter>
             <Route component={Home} exact path="/"></Route>
+            <Route component={CreatePoint} exact path="/create-point"></Route>
         </BrowserRouter>
     )
 }
